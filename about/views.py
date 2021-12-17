c349ba719e4627658ed8bc3af4fc2bcf706cc055
Max <youneedmax@gmail.com>
2021-12-17 15:24:55 +0100

    add Profile model and api endpoint to retrieve it

--- about/views.py
+++ about/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from . import models, serializers
 
@@ -6,3 +8,11 @@ from . import models, serializers
 class AboutUnitList(ListAPIView):
     queryset = models.AboutUnit.objects.all()
     serializer_class = serializers.AboutUnitSerializer
+
+
+class ProfileDetail(APIView):
+
+    def get(self, request):
+        profile = models.Profile.objects.first()
+        serializer = serializers.ProfileSerializer(profile, context={"request": request})
+        return Response(serializer.data)
