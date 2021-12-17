01159b8a5516a678e47aff7010f2383929dee177
Max <youneedmax@gmail.com>
2021-12-17 14:49:23 +0100

    add languages app

---
+++ languages/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Language(models.Model):
+    name = models.CharField("Name", max_length=12)
+    code = models.CharField("Language code", max_length=2)
+    level = models.CharField("Level", max_length=10)
+    learningHistory = models.TextField("Learning history")
+
+    def __str__(self):
+        return self.name
