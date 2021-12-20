fff9d552a6d575d78931ca3e0197806079832976
Max <youneedmax@gmail.com>
2021-12-20 15:27:09 +0100

    add ContactRequest model, serializer and view for it

---
+++ contact/tasks.py
@@ -0,0 +1,11 @@
+import celery
+
+from . import service
+
+
+@celery.shared_task
+def on_new_contact_request(name, email, comment):
+    message = f"<b>New contact request</b>%0A%0A<b>From</b> {name}%0A<b>Email</b> {email}"
+    if comment:
+        message += f"%0A%0A${comment}"
+    service.send_telegram_bot_message(message)
